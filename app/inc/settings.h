7fdd09e913f60759181c6511352eb9f11eca1c20
Vitali Kailevich <vit.kailev@gmail.com>
2023-03-04 22:33:03 +0300

    Created initialization and settingSystemClock functions

--- app/inc/settings.h
+++ app/inc/settings.h
@@ -1,5 +1,8 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include "variables.h"
+
+int initialization(MCUDef *mcu);
 
 #endif //SETTINGS_H
--- app/src/settings.c
+++ app/src/settings.c
@@ -1,9 +1,33 @@
+#include "stm32f4xx_hal_rcc.h"
+
 #include "settings.h"
 
+static int settingSystemClock(void) {
+    RCC_OscInitTypeDef oscInit = {0};
+    oscInit.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
+    oscInit.HSIState = RCC_HSI_ON;
+    oscInit.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    oscInit.LSIState = RCC_LSI_ON;
+    oscInit.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&oscInit) != HAL_OK)
+        return 1;
 
+    RCC_ClkInitTypeDef clkInit = {0};
+    clkInit.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    clkInit.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    clkInit.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    clkInit.APB1CLKDivider = RCC_HCLK_DIV1;
+    clkInit.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&clkInit, FLASH_LATENCY_0) != HAL_OK)
+        return 2;
+    return 0;
+}
 
-
-
+int initialization(MCUDef *mcu) {
+    int32_t result = 0;
+    result |= settingSystemClock();
+    return result;
+}
 
 
 
