95bb8483cbdf9e17a30cf9f360bb709c0e472456
Vitali Kailevich <vit.kailev@gmail.com>
2023-03-05 18:47:32 +0300

    Added timer callback function

--- app/src/callbacks.c
+++ app/src/callbacks.c
@@ -1,9 +1,10 @@
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_tim.h"
 
+#include "variables.h"
 
-
-
-
-
-
-
-
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == ((TIM_HandleTypeDef *) MCU.timer.obj)->Instance) {
+        MCU.timer.isTriggered = true;
+    }
+}
