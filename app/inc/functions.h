82679bc833e63cedcc50ed6f05be03ea3c523791
Vitali Kailevich <vit.kailev@gmail.com>
2023-03-05 18:45:35 +0300

    Added changePinState, getPinState and isTimerTriggered functions

--- app/inc/functions.h
+++ app/inc/functions.h
@@ -1,5 +1,12 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include "variables.h"
+
+void changePinState(PortDef *port, bool state);
+
+bool getPinState(const PortDef *port);
+
+bool isTimerTriggered(const TimerDef *timer);
 
 #endif //FUNCTIONS_H
--- app/src/functions.c
+++ app/src/functions.c
@@ -1,9 +1,18 @@
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+
 #include "functions.h"
 
+void changePinState(PortDef *port, bool state) {
+    HAL_GPIO_WritePin((GPIO_TypeDef *) port->obj, port->pin, state);
+}
 
+bool getPinState(const PortDef *port) {
+    return HAL_GPIO_ReadPin((GPIO_TypeDef *) port->obj, port->pin);
+}
 
-
-
-
+bool isTimerTriggered(const TimerDef *timer) {
+    return timer->isTriggered;
+}
 
 
