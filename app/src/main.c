4850945e59ccbbf48f0011787d273c519aaae2a4
Vitali Kailevich <vit.kailev@gmail.com>
2023-03-12 19:07:02 +0300

    Added base code to work with UART

--- app/src/main.c
+++ app/src/main.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "stm32f4xx_hal.h"
 
 #include "main.h"
@@ -10,18 +12,17 @@ static void startSettings(void);
 
 static void error(uint32_t code);
 
-static uint32_t counter;
-
 int main(void) {
-    uint32_t err = 0;
+    int32_t err = 0;
     startSettings();
     if ((err = HAL_Init()))
         error(err);
-    if ((err = initialization(&MCU)))
+    if ((err = initialization(&MCU, &Terminal)))
         error(err);
-    if ((err = turnOnInterrupts(&MCU)))
+    if ((err = turnOnInterrupts(&MCU, &Terminal)))
         error(err);
 
+    changePinState(&MCU.leds[LED_RED].port, true);
     while (true) {
         application();
     }
@@ -32,24 +33,13 @@ static void application(void) {
     if (isTimerTriggered(&MCU.timer)) {
         MCU.timer.isTriggered = false;
 
-        counter++;
-        if (counter % (MCU.timer.freq / 2) == 0) {
-            MCU.leds[LED_GREEN].state ^= true;
-            changePinState(&MCU.leds[LED_GREEN].port, MCU.leds[LED_GREEN].state);
-        }
-        if (counter % (MCU.timer.freq / 4) == 0) {
-            MCU.leds[LED_ORANGE].state ^= true;
-            changePinState(&MCU.leds[LED_ORANGE].port, MCU.leds[LED_ORANGE].state);
-        }
-        if (counter == MCU.timer.freq) {
-            counter = 0;
-
-            MCU.leds[LED_BLUE].state ^= true;
-            changePinState(&MCU.leds[LED_BLUE].port, MCU.leds[LED_BLUE].state);
-        }
+        changeCounterValue(&MCU);
     }
 
-    changePinState(&MCU.leds[LED_RED].port, getPinState(&MCU.button));
+    updateUART(&Terminal, getCounterValue(&MCU));
+    if (isUARTHaveData(&Terminal)) {
+        Terminal.isHaveData = false;
+    }
 }
 
 static void startSettings(void) {
@@ -64,6 +54,8 @@ static void startSettings(void) {
     MCU.leds[LED_BLUE].port.pin = GPIO_PIN_15;
     MCU.leds[LED_BLUE].port.obj = GPIOD;
     MCU.timer.freq = 100; // Hz
+
+    initUART(&Terminal, 115200);
 }
 
 static void error(uint32_t code) {
