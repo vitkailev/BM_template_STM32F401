c99e756ca0e6728ef4d58a2958e84f424b3feb1f
Vitali Kailevich <vit.kailev@gmail.com>
2023-03-12 19:01:20 +0300

    Added UART callback functions (Tx/Rx/Error)

--- app/src/callbacks.c
+++ app/src/callbacks.c
@@ -1,10 +1,29 @@
 #include "stm32f4xx_hal.h"
-#include "stm32f4xx_hal_tim.h"
 
-#include "variables.h"
+#include "uart.h"
+
+#include "functions.h"
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     if (htim->Instance == ((TIM_HandleTypeDef *) MCU.timer.obj)->Instance) {
         MCU.timer.isTriggered = true;
+    }
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == ((UART_HandleTypeDef *) Terminal.obj)->Instance) {
+        sendUARTByte(&Terminal);
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == ((UART_HandleTypeDef *) Terminal.obj)->Instance) {
+        saveUARTByte(&Terminal, getCounterValue(&MCU));
+    }
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == ((UART_HandleTypeDef *) Terminal.obj)->Instance) {
+        Terminal.error++;
     }
 }
