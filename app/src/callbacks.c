517b7c188382a26bc9cfa92f71d9643383f1ae2a
Vitali Kailevich <vit.kailev@gmail.com>
2023-04-19 00:05:40 +0300

    Added I2C callback functions

--- app/src/callbacks.c
+++ app/src/callbacks.c
@@ -1,6 +1,7 @@
 #include "stm32f4xx_hal.h"
 
 #include "uart.h"
+#include "i2c.h"
 
 #include "functions.h"
 
@@ -25,5 +26,23 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == ((UART_HandleTypeDef *) Terminal.obj)->Instance) {
         Terminal.error++;
+    }
+}
+
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    if (hi2c->Instance == ((I2C_HandleTypeDef *) I2CBus1.obj)->Instance) {
+        I2CBus1.isWriting = false;
+    }
+}
+
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    if (hi2c->Instance == ((I2C_HandleTypeDef *) I2CBus1.obj)->Instance) {
+        I2CBus1.isReading = false;
+    }
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c) {
+    if (hi2c->Instance == ((I2C_HandleTypeDef *) I2CBus1.obj)->Instance) {
+        I2CBus1.error++;
     }
 }
