64273d76d3d311566cbb467246908f50fee5cb10
Vitali Kailevich <vit.kailev@gmail.com>
2024-01-03 01:17:01 +0100

    Change the type of the application, startSettings and error functions

--- app/inc/main.h
+++ app/inc/main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void application(void);
+
+void startSettings(void);
+
+void error(int32_t errCode);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //MAIN_H
--- app/src/main.c
+++ app/src/main.c
@@ -6,18 +6,12 @@
 #include "settings.h"
 #include "functions.h"
 
-static void application(void);
-
-static void startSettings(void);
-
-static void error(uint32_t code);
-
 int main(void) {
     int32_t err = 0;
     startSettings();
     if ((err = HAL_Init()))
         error(err);
-    if ((err = initialization(&MCU, &Terminal)))
+    if ((err = initialization(&MCU, &Terminal, &I2C3Bus)))
         error(err);
     if ((err = turnOnInterrupts(&MCU, &Terminal)))
         error(err);
@@ -29,22 +23,32 @@ int main(void) {
     return 1;
 }
 
-static void application(void) {
+void application(void) {
     if (isTimerTriggered(&MCU.timer)) {
         MCU.timer.isTriggered = false;
 
         changeCounterValue(&MCU);
+        checkButtonState(&MCU.button);
+
+        // Usual time page write - 5 ms
+        updateI2CBus(&I2C3Bus);
+    }
+
+    if (isButtonPressed(&MCU.button)) {
+        MCU.button.isTriggered = false;
     }
 
     updateUART(&Terminal, getCounterValue(&MCU));
     if (isUARTHaveData(&Terminal)) {
         Terminal.isHaveData = false;
+
+        writeUARTData(&Terminal, Terminal.rxBuffer, Terminal.rxSize);
     }
 }
 
-static void startSettings(void) {
-    MCU.button.pin = GPIO_PIN_0;
-    MCU.button.obj = GPIOA;
+void startSettings(void) {
+    MCU.button.port.pin = GPIO_PIN_0;
+    MCU.button.port.obj = GPIOA;
     MCU.leds[LED_GREEN].port.pin = GPIO_PIN_12;
     MCU.leds[LED_GREEN].port.obj = GPIOD;
     MCU.leds[LED_ORANGE].port.pin = GPIO_PIN_13;
@@ -58,7 +62,7 @@ static void startSettings(void) {
     initUART(&Terminal, 115200);
 }
 
-static void error(uint32_t code) {
+void error(int32_t errCode) {
     while (true) {
 
     }
