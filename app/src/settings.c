3134c7476148f558e6b93522712fed7712fab3ef
Vitali Kailevich <vit.kailev@gmail.com>
2026-01-27 11:16:15 +0100

    Update the project settings (Msp and others)

--- app/src/settings.c
+++ app/src/settings.c
@@ -2,35 +2,63 @@
 
 #include "settings.h"
 
-static TIM_HandleTypeDef tim9Handle;
-static UART_HandleTypeDef uart2Handle;
-static I2C_HandleTypeDef i2c3Handle;
+static TIM_HandleTypeDef timer4Handle;
+static TIM_HandleTypeDef timer11Handle;
+static ADC_HandleTypeDef adcHandle;
+static UART_HandleTypeDef uart1Handle;
 static CRC_HandleTypeDef crcHandle;
+static IWDG_HandleTypeDef wdtHandle;
 
+/**
+ * @brief Setting system clocks
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
 static int settingSystemClock(void) {
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
     RCC_OscInitTypeDef oscInit = {0};
     oscInit.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
+    oscInit.LSEState = RCC_LSE_OFF;
+    oscInit.HSEState = RCC_HSE_OFF;
+    oscInit.LSIState = RCC_LSI_ON;
     oscInit.HSIState = RCC_HSI_ON;
     oscInit.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    oscInit.LSIState = RCC_LSI_ON;
-    oscInit.PLL.PLLState = RCC_PLL_NONE;
+
+    // Datasheet, DS9716 Rev. 11, Electrical characteristics, PLL characteristics, page 81
+    oscInit.PLL.PLLState = RCC_PLL_ON;
+    oscInit.PLL.PLLSource = RCC_PLLSOURCE_HSI; // 16MHz
+    oscInit.PLL.PLLM = 16;
+    oscInit.PLL.PLLN = 288; // 288MHz
+    oscInit.PLL.PLLP = RCC_PLLP_DIV4; // 72MHz
+    oscInit.PLL.PLLQ = 6; // 48MHz
+
     if (HAL_RCC_OscConfig(&oscInit) != HAL_OK)
-        return 1;
+        return SETTING_ERROR;
 
     RCC_ClkInitTypeDef clkInit = {0};
     clkInit.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-    clkInit.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    clkInit.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    clkInit.APB1CLKDivider = RCC_HCLK_DIV1;
-    clkInit.APB2CLKDivider = RCC_HCLK_DIV1;
-    if (HAL_RCC_ClockConfig(&clkInit, FLASH_LATENCY_0) != HAL_OK)
-        return 2;
-    return 0;
+    clkInit.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+
+    // Datasheet, DS9716 Rev. 11, Electrical characteristics, Analog-to-digital converter characteristics, page 104
+    // Vdda >= 2.4V, max 36MHz
+    clkInit.AHBCLKDivider = RCC_SYSCLK_DIV1; // 72MHz
+    clkInit.APB1CLKDivider = RCC_HCLK_DIV2; // 36MHz
+    clkInit.APB2CLKDivider = RCC_HCLK_DIV2; // 36MHz
+
+    if (HAL_RCC_ClockConfig(&clkInit, FLASH_LATENCY_2) != HAL_OK)
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
 }
 
+/**
+ * @brief Setting GPIO pins (input and output)
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
 static int settingGPIO(void) {
     GPIO_InitTypeDef gpioInit = {0};
 
+    // Button
     __HAL_RCC_GPIOA_CLK_ENABLE();
     gpioInit.Pin = GPIO_PIN_0;
     gpioInit.Mode = GPIO_MODE_INPUT;
@@ -38,35 +66,152 @@ static int settingGPIO(void) {
     gpioInit.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &gpioInit);
 
+    // LEDs
     __HAL_RCC_GPIOD_CLK_ENABLE();
     gpioInit.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
     gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
     gpioInit.Pull = GPIO_PULLDOWN;
     gpioInit.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOD, &gpioInit);
-    return 0;
+
+    return SETTING_SUCCESS;
 }
 
-static int settingTimer(TimerDef *timer) {
-    timer->obj = &tim9Handle;
-    TIM_HandleTypeDef *timInit = (TIM_HandleTypeDef *) timer->obj;
-    timInit->Instance = TIM9;
-    timInit->Init.Period = timer->freq;
-    timInit->Init.Prescaler = SystemCoreClock / timer->freq / 100 - 1;
+/**
+ * @brief Setting general purpose timer
+ * @param t is the base timer data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+static int settingTimer(TimerDef *t) {
+    uint32_t sourceClock = HAL_RCC_GetPCLK2Freq();
+    // APB2Divider != 1
+    sourceClock *= 2;
+
+    t->handle = (void *) &timer11Handle;
+    t->freq = 4000;
+    t->basePrescaler = t->currentPrescaler = 180 - 1;
+
+    TIM_HandleTypeDef *timInit = (TIM_HandleTypeDef *) t->handle;
+    timInit->Instance = TIM11;
+    timInit->Init.Period = 100 - 1;
+    timInit->Init.Prescaler = t->basePrescaler;
     timInit->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     timInit->Init.CounterMode = TIM_COUNTERMODE_UP;
     timInit->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
     timInit->Init.RepetitionCounter = 0;
     if (HAL_TIM_Base_Init(timInit) != HAL_OK)
-        return 1;
-    return 0;
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
 }
 
+/**
+ * @brief Setting analog-to-digital converter (ADC)
+ * @param adc is the base ADC data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+static int settingADC(AdcDef *adc) {
+    adc->handle = (void *) &adcHandle;
+
+    ADC_HandleTypeDef *adcInit = (ADC_HandleTypeDef *) adc->handle;
+    adcInit->Instance = ADC1;
+
+    // clock source - APB2
+    adcInit->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+
+    adcInit->Init.Resolution = ADC_RESOLUTION_12B;
+    adcInit->Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    adcInit->Init.ScanConvMode = ENABLE;
+    adcInit->Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    adcInit->Init.ContinuousConvMode = DISABLE;
+    adcInit->Init.NbrOfConversion = 3;
+    adcInit->Init.DiscontinuousConvMode = ENABLE;
+    adcInit->Init.NbrOfDiscConversion = 1;
+    adcInit->Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    adcInit->Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+    adcInit->Init.DMAContinuousRequests = DISABLE;
+    if (HAL_ADC_Init(adcInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    ADC_ChannelConfTypeDef chInit = {0};
+
+    // sampling time ( + 12 cycles, RM0368 Rev.6, section 11.5, page 222)
+    chInit.SamplingTime = ADC_SAMPLETIME_144CYCLES; // 8.6667 us
+    chInit.Offset = 0;
+
+    // PA1, ADC1
+    chInit.Channel = ADC_CHANNEL_1;
+    chInit.Rank = 1;
+    if (HAL_ADC_ConfigChannel(adcInit, &chInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    // PB0, ADC1
+    chInit.Channel = ADC_CHANNEL_8;
+    chInit.Rank = 2;
+    if (HAL_ADC_ConfigChannel(adcInit, &chInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    // Temperature sensor, ADC1
+    // Datasheet DS9716, 6.3.21 ,Ts_temp > 10us (accuracy - 1C)
+    chInit.Channel = ADC_CHANNEL_18;
+    chInit.Rank = 3;
+    if (HAL_ADC_ConfigChannel(adcInit, &chInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
+}
+
+/**
+ * @brief Setting PWM output signal
+ * @param t is the base timer data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+static int settingPWM(TimerDef *t) {
+    uint32_t sourceClock = HAL_RCC_GetPCLK1Freq();
+    // APB1Divider != 1
+    sourceClock *= 2;
+
+    t->handle = (void *) &timer4Handle;
+    t->freq = 10000;
+    t->basePrescaler = t->currentPrescaler = 72 - 1;
+
+    TIM_HandleTypeDef *timInit = (TIM_HandleTypeDef *) t->handle;
+    timInit->Instance = TIM4;
+    timInit->Init.Period = 100 - 1;
+    timInit->Init.Prescaler = t->basePrescaler;
+    timInit->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    timInit->Init.CounterMode = TIM_COUNTERMODE_UP;
+    timInit->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    timInit->Init.RepetitionCounter = 0;
+    if (HAL_TIM_PWM_Init(timInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    TIM_OC_InitTypeDef pwm = {0};
+    pwm.OCMode = TIM_OCMODE_PWM1;
+    pwm.OCPolarity = TIM_OCPOLARITY_HIGH;
+    pwm.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+    pwm.OCFastMode = TIM_OCFAST_DISABLE;
+    pwm.OCIdleState = TIM_OCIDLESTATE_RESET;
+    pwm.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+
+    pwm.Pulse = 5;
+    if (HAL_TIM_PWM_ConfigChannel(timInit, &pwm, TIM_CHANNEL_3) != HAL_OK)
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
+}
+
+/**
+ * @brief Setting UART modules
+ * @param uart is the UartDef data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
 static int settingUART(UARTDef *uart) {
-    uart->obj = &uart2Handle;
-    UART_HandleTypeDef *uartInit = (UART_HandleTypeDef *) uart->obj;
-    uartInit->Instance = USART2;
-    uartInit->Init.BaudRate = uart->speed;
+    uart->handle = (void *) &uart1Handle;
+
+    UART_HandleTypeDef *uartInit = (UART_HandleTypeDef *) uart->handle;
+    uartInit->Instance = USART1;
+    uartInit->Init.BaudRate = 115200;
     uartInit->Init.WordLength = UART_WORDLENGTH_8B;
     uartInit->Init.StopBits = UART_STOPBITS_1;
     uartInit->Init.Parity = UART_PARITY_NONE;
@@ -74,49 +219,62 @@ static int settingUART(UARTDef *uart) {
     uartInit->Init.HwFlowCtl = UART_HWCONTROL_NONE;
     uartInit->Init.OverSampling = UART_OVERSAMPLING_8;
     if (HAL_UART_Init(uartInit) != HAL_OK)
-        return 1;
-    return 0;
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
 }
 
-static int settingI2C(I2CDef *i2c) {
-    i2c->obj = &i2c3Handle;
-    I2C_HandleTypeDef *i2cInit = (I2C_HandleTypeDef *) i2c->obj;
-    i2cInit->Init.ClockSpeed = 100000;
-    i2cInit->Init.DutyCycle = I2C_DUTYCYCLE_2;
-    i2cInit->Init.OwnAddress1 = 11;
-    i2cInit->Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-    i2cInit->Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-    i2cInit->Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-    i2cInit->Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-    if (HAL_I2C_Init(i2cInit) != HAL_OK)
-        return 1;
-    return 0;
-}
+/**
+ * @brief Setting Cyclic-Redundancy-Check (CRC) module
+ * @param mcu is the base MCU data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+static int settingCRC(McuDef *mcu) {
+    mcu->handles.crc = (void *) &crcHandle;
 
-static int settingCRC(MCUDef *mcu) {
-    mcu->crc = &crcHandle;
-    CRC_HandleTypeDef *crcInit = (CRC_HandleTypeDef *) mcu->crc;
+    CRC_HandleTypeDef *crcInit = (CRC_HandleTypeDef *) mcu->handles.crc;
     crcInit->Instance = CRC;
     if (HAL_CRC_Init(crcInit) != HAL_OK)
-        return 1;
-    return 0;
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
 }
 
-int initialization(MCUDef *mcu, UARTDef *uart, I2CDef *i2c) {
-    int32_t result = 0;
-    result |= settingSystemClock();
-    settingGPIO();
-    result |= (settingTimer(&mcu->timer) << 1);
-    result |= (settingUART(uart) << 2);
-    result |= (settingI2C(i2c) << 3);
-    result |= (settingCRC(mcu) << 4);
-    return result;
+/**
+ * @brief Setting watchdog timer (WDT) module
+ * @param mcu is the base MCU data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+static int settingWDT(McuDef *mcu) {
+    mcu->handles.wdt = (void *) &wdtHandle;
+
+    IWDG_HandleTypeDef *wdtInit = (IWDG_HandleTypeDef *) mcu->handles.wdt;
+    wdtInit->Instance = IWDG;
+    wdtInit->Init.Prescaler = IWDG_PRESCALER_8;
+    wdtInit->Init.Reload = 0x0FFF;
+    if (HAL_IWDG_Init(wdtInit) != HAL_OK)
+        return SETTING_ERROR;
+
+    return SETTING_SUCCESS;
 }
 
-int turnOnInterrupts(MCUDef *mcu, UARTDef *uart) {
-    if (HAL_TIM_Base_Start_IT((TIM_HandleTypeDef *) mcu->timer.obj) != HAL_OK)
-        return 1;
-    if (HAL_UART_Receive_IT((UART_HandleTypeDef *) uart->obj, &uart->rxByte, 1) != HAL_OK)
-        return 2;
-    return 0;
+/**
+ * @brief Run the MCU modules setting process
+ * @param mcu is the base MCU data structure
+ * @return SETTING_SUCCESS or SETTING_ERROR
+ */
+int initialization(McuDef *mcu) {
+    if (SETTING_SUCCESS != settingSystemClock()) {
+    } else if (SETTING_SUCCESS != settingGPIO()) {
+    } else if (SETTING_SUCCESS != settingTimer(&mcu->measTimer)) {
+    } else if (SETTING_SUCCESS != settingADC(&mcu->adc)) {
+    } else if (SETTING_SUCCESS != settingPWM(&mcu->pwmTimer)) {
+    } else if (SETTING_SUCCESS != settingUART(&mcu->uart)) {
+    } else if (SETTING_SUCCESS != settingCRC(mcu)) {
+    } else if (SETTING_SUCCESS != settingWDT(mcu)) {
+    } else {
+        return SETTING_SUCCESS;
+    }
+
+    return SETTING_ERROR;
 }
--- app/src/stm32f4xx_hal_msp.c
+++ app/src/stm32f4xx_hal_msp.c
@@ -49,7 +49,7 @@
   */
 void HAL_MspInit(void) {
     __HAL_RCC_PWR_CLK_ENABLE();
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
 }
 
 /**
@@ -59,130 +59,169 @@ void HAL_MspInit(void) {
   * @retval None
   */
 void HAL_MspDeInit(void) {
-
 }
 
+/**
+ * @brief Initialize the base timers, turn ON a clock source and setup interrupt vector
+ * @param htim is the pointer to the data structure of the base timer handle (HAL).
+ */
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM9) {
-        __HAL_RCC_TIM9_CLK_ENABLE();
-        HAL_NVIC_SetPriority(TIM1_BRK_TIM9_IRQn, 5, 0);
-        HAL_NVIC_EnableIRQ(TIM1_BRK_TIM9_IRQn);
+    if (htim->Instance == TIM11) {
+        __HAL_RCC_TIM11_CLK_ENABLE();
+
+        HAL_NVIC_SetPriority(TIM1_TRG_COM_TIM11_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(TIM1_TRG_COM_TIM11_IRQn);
     }
 }
 
+/**
+ * @brief DeInitialize the base timers
+ * @param htim is the pointer to the data structure of the base timer handle (HAL).
+ */
 void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM9) {
-        __HAL_RCC_TIM9_FORCE_RESET();
-        __HAL_RCC_TIM9_RELEASE_RESET();
-        HAL_NVIC_DisableIRQ(TIM1_BRK_TIM9_IRQn);
+    if (htim->Instance == TIM11) {
+        __HAL_RCC_TIM11_FORCE_RESET();
+        __HAL_RCC_TIM11_RELEASE_RESET();
+        __HAL_RCC_TIM11_CLK_DISABLE();
+
+        HAL_NVIC_DisableIRQ(TIM1_TRG_COM_TIM11_IRQn);
     }
 }
 
+/**
+ * @brief Initialize the PWM timer mode, turn ON a clock source and setup GPIOs
+ * @param htim is the pointer to the data structure of the timer handle (HAL).
+ */
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim) {
+    GPIO_InitTypeDef gpioInit = {0};
+
+    if (htim->Instance == TIM4) {
+        __HAL_RCC_TIM4_CLK_ENABLE();
+
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        gpioInit.Pin = GPIO_PIN_8;
+        gpioInit.Mode = GPIO_MODE_AF_PP;
+        gpioInit.Pull = GPIO_PULLDOWN;
+        gpioInit.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        gpioInit.Alternate = GPIO_AF2_TIM4;
+        HAL_GPIO_Init(GPIOB, &gpioInit);
+    }
+}
+
+/**
+ * @brief DeInitialize the PWM timer mode
+ * @param htim is the pointer to the data structure of the timer handle (HAL).
+ */
+void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM4) {
+        __HAL_RCC_TIM4_FORCE_RESET();
+        __HAL_RCC_TIM4_RELEASE_RESET();
+        __HAL_RCC_TIM4_CLK_DISABLE();
+
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8);
+    }
+}
+
+/**
+ * @brief Initialize the ADC module, turn ON a clock source, setup GPIO and interrupt vector
+ * @param hadc is the pointer to the data structure of the ADC handle (HAL).
+ */
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
+    GPIO_InitTypeDef gpioInit = {0};
+    gpioInit.Mode = GPIO_MODE_ANALOG;
+    gpioInit.Pull = GPIO_NOPULL;
+
+    if (hadc->Instance == ADC1) {
+        __HAL_RCC_ADC1_CLK_ENABLE();
+
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        gpioInit.Pin = GPIO_PIN_0;
+        HAL_GPIO_Init(GPIOB, &gpioInit);
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        gpioInit.Pin = GPIO_PIN_1;
+        HAL_GPIO_Init(GPIOA, &gpioInit);
+
+        HAL_NVIC_SetPriority(ADC_IRQn, 3, 0);
+        HAL_NVIC_EnableIRQ(ADC_IRQn);
+    }
+}
+
+/**
+ * @brief DeInitialize the ADC module
+ * @param hadc is the pointer to the data structure of the ADC handle (HAL).
+ */
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc) {
+    if (hadc->Instance == ADC1) {
+        __HAL_RCC_ADC_FORCE_RESET();
+        __HAL_RCC_ADC_RELEASE_RESET();
+        __HAL_RCC_ADC1_CLK_DISABLE();
+
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0);
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
+
+        HAL_NVIC_DisableIRQ(ADC_IRQn);
+    }
+}
+
+/**
+ * @brief Initialize the UART interfaces, turn ON a clock source, setup GPIO and interrupt vector
+ * @param huart is the pointer to the data structure of the UART handle (HAL).
+ */
 void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
-    if (huart->Instance == USART2) {
-        __HAL_RCC_USART2_CLK_ENABLE();
+    GPIO_InitTypeDef initGPIO = {0};
 
-        __HAL_RCC_GPIOA_CLK_ENABLE();
+    if (huart->Instance == USART1) {
+        __HAL_RCC_USART1_CLK_ENABLE();
 
-        GPIO_InitTypeDef initGPIO;
-        initGPIO.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        initGPIO.Pin = GPIO_PIN_6 | GPIO_PIN_7;
         initGPIO.Mode = GPIO_MODE_AF_PP;
-        initGPIO.Pull = GPIO_NOPULL;
-        initGPIO.Speed = GPIO_SPEED_FREQ_MEDIUM;
-        initGPIO.Alternate = GPIO_AF7_USART2;
-        HAL_GPIO_Init(GPIOA, &initGPIO);
-
-        HAL_NVIC_SetPriority(USART2_IRQn, 2, 0);
-        HAL_NVIC_EnableIRQ(USART2_IRQn);
-    }
-}
-
-void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
-    if (huart->Instance == USART2) {
-        __HAL_RCC_USART2_FORCE_RESET();
-        __HAL_RCC_USART2_RELEASE_RESET();
-
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2);
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_3);
-
-        HAL_NVIC_DisableIRQ(USART2_IRQn);
-    }
-}
-
-void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c) {
-    if (hi2c->Instance == I2C3) {
-        __HAL_RCC_I2C3_CLK_ENABLE();
-
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-        __HAL_RCC_GPIOC_CLK_ENABLE();
-
-        GPIO_InitTypeDef initGPIO;
-        initGPIO.Mode = GPIO_MODE_AF_OD;
         initGPIO.Pull = GPIO_PULLUP;
-        initGPIO.Speed = GPIO_SPEED_FREQ_MEDIUM;
-        initGPIO.Alternate = GPIO_AF4_I2C3;
-        initGPIO.Pin = GPIO_PIN_8;
-        HAL_GPIO_Init(GPIOA, &initGPIO);
-        initGPIO.Pin = GPIO_PIN_9;
-        HAL_GPIO_Init(GPIOC, &initGPIO);
+        initGPIO.Speed = GPIO_SPEED_FREQ_HIGH;
+        initGPIO.Alternate = GPIO_AF7_USART1;
+        HAL_GPIO_Init(GPIOB, &initGPIO);
 
-        HAL_NVIC_SetPriority(I2C3_EV_IRQn, 1, 0);
-        HAL_NVIC_EnableIRQ(I2C3_EV_IRQn);
-        HAL_NVIC_SetPriority(I2C3_ER_IRQn, 4, 0);
-        HAL_NVIC_EnableIRQ(I2C3_ER_IRQn);
+        HAL_NVIC_SetPriority(USART1_IRQn, 4, 0);
+        HAL_NVIC_EnableIRQ(USART1_IRQn);
     }
 }
 
-void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c) {
-    if (hi2c->Instance == I2C3) {
-        __HAL_RCC_I2C3_FORCE_RESET();
-        __HAL_RCC_I2C3_RELEASE_RESET();
+/**
+ * @brief DeInitialize the UART interfaces
+ * @param huart is the pointer to the data structure of the UART handle (HAL).
+ */
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        __HAL_RCC_USART1_FORCE_RESET();
+        __HAL_RCC_USART1_RELEASE_RESET();
+        __HAL_RCC_USART1_CLK_DISABLE();
 
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8);
-        HAL_GPIO_DeInit(GPIOC, GPIO_PIN_9);
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6);
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_7);
 
-        HAL_NVIC_DisableIRQ(I2C3_EV_IRQn);
-        HAL_NVIC_DisableIRQ(I2C3_ER_IRQn);
+        HAL_NVIC_DisableIRQ(USART1_IRQn);
     }
 }
 
+/**
+ * @brief Initialize the CRC module, turn ON a clock source
+ * @param hcrc is the pointer to the data structure of the CRC handle (HAL).
+ */
 void HAL_CRC_MspInit(CRC_HandleTypeDef *hcrc) {
-    __HAL_RCC_CRC_CLK_ENABLE();
+    if (hcrc->Instance == CRC) {
+        __HAL_RCC_CRC_CLK_ENABLE();
+    }
 }
 
+/**
+ * @brief DeInitialize the CRC module
+ * @param hcrc is the pointer to the data structure of the CRC handle (HAL).
+ */
 void HAL_CRC_MspDeInit(CRC_HandleTypeDef *hcrc) {
-    __HAL_RCC_CRC_FORCE_RESET();
-    __HAL_RCC_CRC_RELEASE_RESET();
+    if (hcrc->Instance == CRC) {
+        __HAL_RCC_CRC_FORCE_RESET();
+        __HAL_RCC_CRC_RELEASE_RESET();
+        __HAL_RCC_CRC_CLK_DISABLE();
+    }
 }
-
-/**
-  * @brief  Initializes the PPP MSP.
-  * @note   This functiona is called from HAL_PPP_Init() function to perform 
-  *         peripheral(PPP) system level initialization (GPIOs, clock, DMA, interrupt)
-  * @retval None
-  */
-void HAL_PPP_MspInit(void) {
-
-}
-
-/**
-  * @brief  DeInitializes the PPP MSP.
-  * @note   This functiona is called from HAL_PPP_DeInit() function to perform 
-  *         peripheral(PPP) system level de-initialization (GPIOs, clock, DMA, interrupt)
-  * @retval None
-  */
-void HAL_PPP_MspDeInit(void) {
-
-}
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
