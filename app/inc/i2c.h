04527662cdfbfb14576c5b7691c4e810c8b16fc2
Vitali Kailevich <vit.kailev@gmail.com>
2026-04-05 20:54:58 +0200

    Update the I2C interface driver

--- app/inc/i2c.h
+++ app/inc/i2c.h
@@ -5,52 +5,52 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stddef.h>
 
 enum I2C_Constants {
-    I2C_PAGE_SIZE = 128, // in EEPROM
-    I2C_ADDRESS_SIZE = 4,
-    I2C_BUFFER_SIZE = I2C_PAGE_SIZE + I2C_ADDRESS_SIZE,
+    I2C_BUFFER_SIZE = 64
+};
+
+enum I2C_Errors {
+    I2C_SUCCESS = 0,
+    I2C_NOT_INIT = -1,
+    I2C_BUSY = -2,
+    I2C_WRONG_DATA = -3,
+    I2C_HW_ERROR = -4,
+
+    I2C_NUMBER_ERRORS = 4
 };
 
 typedef struct {
-    bool isBusy;
-    bool isReading;
-    bool isWriting;
+    bool isInit;
+    volatile bool isReading;
+    volatile bool isWriting;
+    volatile uint32_t errType;
+    volatile uint32_t errors;
 
-    uint8_t deviceAdd;
-    uint32_t memoryAdd;
+    uint16_t deviceAdd;
 
-    uint16_t rxSize;
-    uint16_t txSize;
-    void *rxData;
-    const void *txData;
+    uint16_t size;
     uint8_t buffer[I2C_BUFFER_SIZE];
 
-    uint32_t error;
-
-    void *obj;
+    void *handle;
 } I2CDef;
 
-extern I2CDef I2CBus1;
+int I2C_init(I2CDef *i2c);
 
-bool isI2CBusy(const I2CDef *i2c);
+bool I2C_isReading(const I2CDef *i2c);
 
-void takeI2CBus(I2CDef *i2c);
+int I2C_readData(I2CDef *i2c, uint16_t devAdd, uint16_t size);
 
-void freeI2CBus(I2CDef *i2c);
+const uint8_t *I2C_getData(const I2CDef *i2c);
 
-bool isI2CReading(const I2CDef *i2c);
+uint16_t I2C_getDataSize(const I2CDef *i2c);
 
-int readI2CData(I2CDef *i2c, uint8_t devAdd, uint32_t memAdd, uint8_t addSize, void *data, uint16_t size);
+bool I2C_isWriting(const I2CDef *i2c);
 
-bool isI2CWriting(const I2CDef *i2c);
-
-int writeI2CData(I2CDef *i2c, uint8_t devAdd, uint32_t memAdd, uint8_t addSize, const void *data, uint16_t size);
-
-void updateI2CBus(I2CDef *i2c);
+int I2C_writeData(I2CDef *i2c, uint16_t devAdd, const void *data, uint16_t size, bool isNeedStop);
 
 #ifdef __cplusplus
 }
--- app/src/i2c.c
+++ app/src/i2c.c
@@ -4,129 +4,128 @@
 
 #include "i2c.h"
 
-bool isI2CBusy(const I2CDef *i2c) {
-    return i2c->isBusy;
+/**
+ * @brief Check, that the I2C interface is initialized
+ * @param i2c is the base I2C data structure
+ * @return True - I2C interface has been initialized, otherwise - False
+ */
+static bool isInit(const I2CDef *i2c) {
+    return i2c->isInit;
 }
 
-void takeI2CBus(I2CDef *i2c) {
-    i2c->isBusy = true;
+/**
+ * @brief I2C interface initialization
+ * @param i2c is the base I2C data structure
+ * @return I2C_SUCCESS - the data structure has been initialized, otherwise - error code
+ */
+int I2C_init(I2CDef *i2c) {
+    if (i2c == NULL)
+        return I2C_WRONG_DATA;
+
+    i2c->errType = HAL_I2C_ERROR_NONE;
+    i2c->errors = 0;
+    memset(i2c->buffer, 0, I2C_BUFFER_SIZE);
+
+    i2c->isInit = true;
+    return I2C_SUCCESS;
 }
 
-void freeI2CBus(I2CDef *i2c) {
-    i2c->isBusy = false;
-}
-
-bool isI2CReading(const I2CDef *i2c) {
+/**
+ * @brief Check, that the I2C interface is reading data
+ * @param i2c is the base I2C data structure
+ * @return True - is reading, otherwise - False
+ */
+bool I2C_isReading(const I2CDef *i2c) {
     return i2c->isReading;
 }
 
-int readI2CData(I2CDef *i2c, uint8_t devAdd, uint32_t memAdd, uint8_t addSize, void *data, uint16_t size) {
-    if (!isI2CBusy(i2c))
-        return 1;
-    if (isI2CReading(i2c) || isI2CWriting(i2c))
-        return 2;
-    if (data == NULL || size == 0 || size > I2C_BUFFER_SIZE)
-        return 3;
+/**
+ * @brief Reading data via I2C interface into an internal buffer
+ * @param i2c is the base I2C data structure
+ * @param devAdd is the device address on the I2C bus
+ * @param size is the amount of data need to be read (bytes)
+ * @return I2C_Errors value
+ */
+int I2C_readData(I2CDef *i2c, uint16_t devAdd, uint16_t size) {
+    if (!isInit(i2c))
+        return I2C_NOT_INIT;
+    if (I2C_isReading(i2c) || I2C_isWriting(i2c))
+        return I2C_BUSY;
+    if (size == 0 || size > I2C_BUFFER_SIZE)
+        return I2C_WRONG_DATA;
 
     i2c->isReading = true;
     i2c->deviceAdd = devAdd;
-    i2c->memoryAdd = memAdd;
-    i2c->rxData = data;
-    i2c->rxSize = size;
-    switch (addSize) {
-        case 1:
-            i2c->buffer[0] = i2c->memoryAdd;
-            break;
-        case 2:
-            i2c->buffer[0] = i2c->memoryAdd;
-            i2c->buffer[1] = i2c->memoryAdd >> 8;
-            break;
-        case 4:
-            i2c->buffer[0] = i2c->memoryAdd;
-            i2c->buffer[1] = i2c->memoryAdd >> 8;
-            i2c->buffer[2] = i2c->memoryAdd >> 16;
-            i2c->buffer[3] = i2c->memoryAdd >> 24;
-            break;
-        default:
-            break;
-    }
+    memset(i2c->buffer, 0, I2C_BUFFER_SIZE);
+    i2c->size = size;
+    i2c->errType = HAL_I2C_ERROR_NONE;
 
-    //MCU specific code
-    HAL_I2C_Master_Transmit_IT((I2C_HandleTypeDef *) i2c->obj, i2c->deviceAdd, i2c->buffer, addSize);
-    return 0;
+    HAL_StatusTypeDef result = HAL_ERROR;
+    result = HAL_I2C_Master_Seq_Receive_IT((I2C_HandleTypeDef *) i2c->handle, i2c->deviceAdd,
+                                           i2c->buffer, i2c->size,
+                                           I2C_LAST_FRAME);
+
+    return (result == HAL_OK) ? I2C_SUCCESS : I2C_HW_ERROR;
 }
 
-bool isI2CWriting(const I2CDef *i2c) {
+/**
+ * @brief Get a pointer to the received data
+ * @param i2c is the base I2C data structure
+ * @return pointer to the internal buffer
+ */
+const uint8_t *I2C_getData(const I2CDef *i2c) {
+    return i2c->buffer;
+}
+
+/**
+ * @brief Get the size of the received data
+ * @param i2c is the base I2C data structure
+ * @return an amount of available data (bytes)
+ */
+uint16_t I2C_getDataSize(const I2CDef *i2c) {
+    return i2c->size;
+}
+
+/**
+ * @brief Check, that the I2C interface is sending data
+ * @param i2c is the base I2C data structure
+ * @return True - is writing, otherwise - False
+ */
+bool I2C_isWriting(const I2CDef *i2c) {
     return i2c->isWriting;
 }
 
-int writeI2CData(I2CDef *i2c, uint8_t devAdd, uint32_t memAdd, uint8_t addSize, const void *data, uint16_t size) {
-    if (!isI2CBusy(i2c))
-        return 1;
-    if (isI2CReading(i2c) || isI2CWriting(i2c))
-        return 2;
-    if (data == NULL || size == 0 || size > I2C_BUFFER_SIZE)
-        return 3;
+/**
+ * @brief Sending data via I2C interface
+ * @param i2c is the base I2C data structure
+ * @param devAdd is the device address on the I2C bus
+ * @param src is the pointer of the source data
+ * @param size is the size of the source data (bytes)
+ * @param isNeedStop is a flag, that indicates whether a "Stop condition" should be generated after a transaction or not
+ * @return I2C_Errors code
+ */
+int I2C_writeData(I2CDef *i2c, uint16_t devAdd, const void *src, uint16_t size, bool isNeedStop) {
+    if (!isInit(i2c))
+        return I2C_NOT_INIT;
+    if (I2C_isReading(i2c) || I2C_isWriting(i2c))
+        return I2C_BUSY;
+    if (src == NULL || size == 0 || size > I2C_BUFFER_SIZE)
+        return I2C_WRONG_DATA;
 
     i2c->isWriting = true;
     i2c->deviceAdd = devAdd;
-    i2c->memoryAdd = memAdd;
-    i2c->txData = data;
-    i2c->txSize = size;
-    switch (addSize) {
-        case 1:
-            i2c->buffer[0] = i2c->memoryAdd;
-            break;
-        case 2:
-            i2c->buffer[0] = i2c->memoryAdd;
-            i2c->buffer[1] = i2c->memoryAdd >> 8;
-            break;
-        case 4:
-            i2c->buffer[0] = i2c->memoryAdd;
-            i2c->buffer[1] = i2c->memoryAdd >> 8;
-            i2c->buffer[2] = i2c->memoryAdd >> 16;
-            i2c->buffer[3] = i2c->memoryAdd >> 24;
-            break;
-        default:
-            break;
-    }
+    memcpy((void *) i2c->buffer, src, size);
+    i2c->size = size;
+    i2c->errType = HAL_I2C_ERROR_NONE;
 
-    if (size > I2C_PAGE_SIZE)
-        size = I2C_PAGE_SIZE;
-    memcpy((void *) (i2c->buffer + addSize), i2c->txData, size);
-    i2c->txSize -= size;
-    i2c->txData = (uint8_t *) i2c->txData + size;
+    HAL_StatusTypeDef result = HAL_ERROR;
+    result = HAL_I2C_Master_Seq_Transmit_IT((I2C_HandleTypeDef *) i2c->handle, i2c->deviceAdd,
+                                            i2c->buffer, i2c->size,
+                                            // I2C_FIRST_AND_LAST_FRAME - just send one data sequence
+                                            // I2C_FIRST_FRAME - send a command and then, without a "Stop" condition,
+                                            // read the data (when it is necessary to set the reading address in
+                                            // a device/IC), (clock stretching)
+                                            (isNeedStop) ? I2C_FIRST_AND_LAST_FRAME : I2C_FIRST_FRAME);
 
-    //MCU specific code
-    HAL_I2C_Master_Transmit_IT((I2C_HandleTypeDef *) i2c->obj, i2c->deviceAdd, i2c->buffer, addSize + size);
-    return 0;
-}
-
-void updateI2CBus(I2CDef *i2c) {
-    if (!isI2CBusy(i2c))
-        return;
-    if (isI2CReading(i2c) || isI2CWriting(i2c))
-        return;
-
-    uint16_t size = 0;
-    //MCU specific code
-    if (i2c->rxSize) {
-        i2c->isReading = true;
-
-        size = (i2c->rxSize > I2C_PAGE_SIZE) ? I2C_PAGE_SIZE : i2c->rxSize;
-        memcpy(i2c->rxData, (const void *) i2c->buffer, size);
-        i2c->rxSize -= size;
-        if (i2c->rxSize)
-            HAL_I2C_Master_Receive_IT((I2C_HandleTypeDef *) i2c->obj, i2c->deviceAdd, i2c->buffer, size);
-    }
-    //MCU specific code
-    if (i2c->txSize) {
-        i2c->isWriting = true;
-
-        const void *data = i2c->txData;
-        size = (i2c->txSize > I2C_PAGE_SIZE) ? I2C_PAGE_SIZE : i2c->txSize;
-        i2c->txSize -= size;
-        i2c->txData = (uint8_t *) i2c->txData + size;
-        HAL_I2C_Master_Transmit_IT((I2C_HandleTypeDef *) i2c->obj, i2c->deviceAdd, (uint8_t *) data, size);
-    }
+    return (result == HAL_OK) ? I2C_SUCCESS : I2C_HW_ERROR;
 }
